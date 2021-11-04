6e889eb0f1a5036ffed94a43f6058e6723d4e247
Daniel Hawn <danieljhawn@gmail.com>
2021-11-04 17:44:41 -0500

    adds input for labels

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const input = document.getElementById('input');
+const labels = document.getElementById('labels');
 const buttonDiv = document.getElementById('buttons');
 let tabData;
 let tracker = 0;
 
 function data() {
-    let data = input.value;
+    let labelTitles = labels.value;
     let rowOrCol = document.getElementById('row-or-col').value;
+    if (rowOrCol == "col") {
+        console.log("col");
+        labelData = labelTitles.split('\n')
+    } else {
+        console.log("row");
+        labelData = labelTitles.split('\t')
+    }
+
+    labelData = labelData.filter(labelTitles => labelTitles.trim() != '');
+        
+    // for (cell in labelData) {
+    //     console.log(labelData[cell]);
+    //     const heading = document.createElement('h4');
+    //     const br = document.createElement('br');
+    //     heading.innerHTML = labelData[cell];
+    //     buttonDiv.append(heading, br);
+    // }
+    
+    let data = input.value;
     if (rowOrCol == "col") {
         console.log("col");
         tabData = data.split('\n')
@@ -15,16 +35,20 @@ function data() {
     }
 
     tabData = tabData.filter(data => data.trim() != '');
+
     
-    for (cell in tabData) {
+    for (cell in tabData, labelData) {
         console.log(tabData[cell]);
+        const heading = document.createElement('h4');
+        const br = document.createElement('br');
+        heading.innerHTML = labelData[cell];
         const paster = document.createElement('textarea');
         const clicker = document.createElement('button');
         clicker.innerHTML = "COPY";
         clicker.addEventListener("click", myScript);
-        const br = document.createElement('br');
         paster.innerHTML = tabData[cell];
-        buttonDiv.append(paster, clicker, br);
+
+        buttonDiv.append(heading, paster, br, clicker, br);
     }
     tracker = 0;
 }
